1f982363bd954bec20ca2c555f3bbefb43586450
thiagomag94 <thiagomag94@gmail.com>
2024-08-25 23:22:05 -0300

    project running

--- acervo-front-end/src/components/Menu/Menu.tsx
+++ acervo-front-end/src/components/Menu/Menu.tsx
@@ -5,7 +5,7 @@ export  const Menu = () => {
 
     const router = useRouter()
     return (
-      <ul className="flex  flex-row items-center justify-start gap-12 p-8  outline-none text-cyan-900 font-semibold bg-gray-100 rounded-full">
+      <ul className="flex  flex-row items-center justify-start gap-12 p-8  outline-none text-cyan-900 font-semibold bg-gray-100 rounded-l-full border border-cyan-700/60">
        <li className="cursor-pointer hover:text-cyan-700 transition ease-in-out delay-75" onClick={()=>router.push('/')}>Home</li>
        <li className="cursor-pointer hover:text-cyan-700 transition ease-in-out delay-75" onClick={()=>router.push('/livros')}>Livros</li>
        <li className="cursor-pointer hover:text-cyan-700 transition ease-in-out delay-75" onClick={()=>router.push('/about')}>Sobre nós</li>
